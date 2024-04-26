6019fc2b815d946165ef5dff3f781d0fd262a501
Bagiev Arslan <arslan.bagiev@gmail.com>
2024-04-26 11:30:31 +0300

    feat: Добавлена возможность получить всех user по ссылке /api/users

--- DAL/Repositories/UserRepository.cs
+++ DAL/Repositories/UserRepository.cs
@@ -6,14 +6,21 @@ namespace DAL.Repositories
     public interface IUserRepository
     {
         int Add(User user);
-
+        List<User> GetUsers();
     }
+
     public class UserRepository(MedicalFileContext context) : Repository<User>(context), IUserRepository
     {
         public int Add(User user)
         {
             Create(user);
             return user.Id;
+        }
+
+        public List<User> GetUsers()
+        {
+            var allUsers = context.Users.ToList();
+            return allUsers;
         }
     }
 }
