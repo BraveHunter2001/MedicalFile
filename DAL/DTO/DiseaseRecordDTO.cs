b2d542b8dade9eedb9abcd10c579966436a06603
Bagiev Arslan <arslan.bagiev@gmail.com>
2024-05-09 14:21:58 +0300

    refactor: Change GET method in diseaseController

--- DAL/DTO/DiseaseRecordDTO.cs
+++ DAL/DTO/DiseaseRecordDTO.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL.Models;
 
-namespace DAL.DTO
+namespace DAL.DTO;
+
+public class DiseaseRecordDTO
 {
-    public class DiseaseRecordDTO
+    public DateTime CreateDate { get; set; }
+    public string DoctorName { get; set; }
+    public string PatientName { get; set; }
+    public string Symptoms { get; set; }
+    public string Anamnesis { get; set; }
+    public string Treatment { get; set; }
+
+    public DiseaseRecordDTO(DiseaseRecord diseaseRecord) 
     {
-        public DateTime CreateDate { get; set; }
-        public string DoctorName { get; set; }
-        public string PatientName { get; set; }
-        public string Symptoms { get; set; }
-        public string Anamnesis { get; set; }
-        public string Treatment { get; set; }
+        CreateDate = diseaseRecord.CreatedDate;
+        DoctorName = diseaseRecord.Doctor.Name;
+        PatientName = diseaseRecord.Patient.Name;
+        Symptoms = diseaseRecord.Symptoms;
+        Anamnesis = diseaseRecord.Anamnesis;
+        Treatment = diseaseRecord.Treatment;
     }
 }
--- DAL/DTO/PatientCharacteristicDTO.cs
+++ DAL/DTO/PatientCharacteristicDTO.cs
@@ -1,15 +1,14 @@
 ﻿using DAL.Models;
 
-namespace DAL.DTO
-{
-    public class PatientCharacteristicDTO
-    { 
-        public int Age { get; set; }
-        public string RiskFactor { get; set; }
-        public PatientCharacteristicDTO(PatientCharacteristic patientCharacteristic)
-        {
-            Age = patientCharacteristic.Age;
-            RiskFactor = patientCharacteristic.RiskFactor;
-        }
+namespace DAL.DTO;
+
+public class PatientCharacteristicDTO
+{ 
+    public int Age { get; set; }
+    public string RiskFactor { get; set; }
+    public PatientCharacteristicDTO(PatientCharacteristic patientCharacteristic)
+    {
+        Age = patientCharacteristic.Age;
+        RiskFactor = patientCharacteristic.RiskFactor;
     }
 }
