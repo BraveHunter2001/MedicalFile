ee42a173be4b4fb8a6b659ca4cd731b862924824
Ilya Myshkin <im@versta24.ru>
2024-05-14 22:32:21 +0300

    feat: added get patients from back

--- webclient/src/Components/Patinets/Patients.js
+++ webclient/src/Components/Patinets/Patients.js
@@ -2,65 +2,13 @@ import { Col, Row } from "reactstrap";
 import CustomTable from "../Table/CustomTable";
 import LoadingButton from "../Buttons/LoadingButton";
 import { useState } from "react";
+import { getAsync } from "../../axiosUtils";
+import { GET_PATIENTS } from "../../constants";
 
 const HEADERS = [
   { title: "Name", name: "name" },
   { title: "Age", name: "age" },
   { title: "RiskFactor", name: "riskFactor" },
-];
-
-const mockItems = [
-  {
-    name: "Chaney Sanford",
-    age: 52,
-    riskFactor:
-      "tempor erat neque non quam. Pellentesque habitant morbi tristique",
-  },
-  {
-    name: "Isaac Arnold",
-    age: 20,
-    riskFactor: "orci. Phasellus dapibus quam quis",
-  },
-  {
-    name: "Tobias Owens",
-    age: 84,
-    riskFactor: "risus.",
-  },
-  {
-    name: "Adam Pickett",
-    age: 63,
-    riskFactor: "aliquet, metus urna convallis erat, eget tincidunt dui",
-  },
-  {
-    name: "Jason Robles",
-    age: 39,
-    riskFactor: "eros. Nam consequat dolor vitae dolor. Donec fringilla. Donec",
-  },
-  {
-    name: "Rhoda Frost",
-    age: 50,
-    riskFactor: "Curabitur massa. Vestibulum accumsan neque et nunc. Quisque",
-  },
-  {
-    name: "Anne Nolan",
-    age: 15,
-    riskFactor: "amet, risus.",
-  },
-  {
-    name: "Adena Campos",
-    age: 70,
-    riskFactor: "velit eget laoreet posuere, enim nisl elementum",
-  },
-  {
-    name: "Pearl Singleton",
-    age: 39,
-    riskFactor: "sociosqu ad litora torquent",
-  },
-  {
-    name: "Dale Reed",
-    age: 22,
-    riskFactor: "scelerisque",
-  },
 ];
 
 const Patients = () => {
@@ -69,10 +17,20 @@ const Patients = () => {
 
   const handleSubmitPatinet = () => {
     setIsLoading(true);
-    setTimeout(() => {
-      setPatients(mockItems);
-      setIsLoading(false);
-    }, 2000);
+    (async () => {
+      const { isOk, data } = await getAsync(GET_PATIENTS);
+      isOk &&
+        data &&
+        setPatients(
+          data?.map((d) => ({
+            name: d.name,
+            age: d.patientCharacteristic.age,
+            riskFactor: d.patientCharacteristic.riskFactor,
+          }))
+        );
+    })();
+
+    setIsLoading(false);
   };
 
   return (
--- webclient/src/constants.js
+++ webclient/src/constants.js
@@ -3,6 +3,8 @@ export const BACK_URL = "http://localhost:5220/";
 export const getSuggestUrl = (promt, role) =>
   `api/suggests/users/?query=${promt}&role=${role}`;
 
+export const GET_PATIENTS = "api/users/patients";
+
 export const MODEL_MODE = {
   Edit: 0,
   View: 1,
